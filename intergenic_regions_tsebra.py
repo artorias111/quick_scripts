c1b0d096671af11109c13d30f48a2fc73313b900
Shriram <alekhshriram@gmail.com>
2023-05-17 13:36:35 -0500

    python script for intergenic regions in a gtf file

---
+++ intergenic_regions_tsebra.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#python script to take a tsebra output, and return the length of intergenic sequences (with coordinates and scaffold names)
+#input : a gtf file, from a tsebra output
+
+#output : a tsv file with `scaffold_name\tstart\tend\tlength\tprevious_gene\tnext_gene

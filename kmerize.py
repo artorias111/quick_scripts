8ffdd7bfb2f34d47a8759a260ad436903cf9f9b6
sb65 <sb65@polar.life.illinois.edu>
2023-05-24 13:52:08 -0500

    kmerize full

--- kmerize.py
+++ kmerize.py
@@ -2,7 +2,7 @@
 #a python script to accept a nucleotide sequence (FASTA) and a k-merize size, and output all the kmers (not just the unique ones, use unixtools later on the output if you want unique k-mers) as a text file
 
 #input : path to a fasta file (string), kmer size (int)
-#output : text file containing all the k-mers
+#output : fasta file containing all the k-mers
 
 import argparse
 
@@ -14,18 +14,27 @@ args=parser.parse_args()
 
 ####process fasta####
 sequence=''
+header_name=''
 my_file=open(args.fasta)
 for i in my_file:
 	raw_string=str(i).strip()
 	if raw_string[0]=='>':
+		header_name=raw_string
 		continue
-	sequence.append(i)
+	sequence+=raw_string
 my_file.close()
 
 #open the output file
+out_file=open('kmerize_out.fa','w')
 
 #rest of the code
 k=args.kmer
 for i in range(len(sequence)-k+1):
 	j=i+k
-	print(sequence[i:j])
+	#print(sequence[i:j])
+	out_file.write(header_name+'_kmer_'+str(i))
+	out_file.write('\n')
+	out_file.write(sequence[i:j])
+	out_file.write('\n')
+
+out_file.close()
